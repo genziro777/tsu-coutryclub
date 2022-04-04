a98232bb723f792313eaa6876d2c7c9487dd4b31
三澤　晴光 <fast.webwk@gmail.com>
2022-04-04 15:10:25 +0900

    あとTOPだけ

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,15 @@ $(".slider-1").slick({
     arrows: false,
     dots: true,//下部ドットナビゲーションの表示
 });
+$(".slider-3").slick({
+    fade:true,//切り替えをフェードで行う。初期値はfalse。
+    // autoplay: true,//自動的に動き出すか。初期値はfalse。
+    autoplaySpeed: 5000,//次のスライドに切り替わる待ち時間
+    speed:1000,//スライドの動きのスピード。初期値は300。
+    arrows: false,
+    dots: true,//下部ドットナビゲーションの表示
+    // dotsClass: 'slide-dots'
+});
 
 /* =============== アコーディオン ============================== */
 $(".accordion__title").on("click", function () {
